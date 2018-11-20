06b8e4beea7c17dd834237b04eb0c375a99da1d5
David Nguyen <davidnguyen@Davids-MacBook-Pro.local>
2018-11-20 08:58:44 -1000

    Fixed background class

--- src/Background.java
+++ src/Background.java
@@ -1,22 +1,22 @@
 // class made by David Nguyen
 public class Background {
-	int x;
-	int y;
+	private int x = 1600;
+	private int y = 800;
 	EZImage picture1;
 	EZImage picture2;
 
 	Background() {
-		picture1 = EZ.addImage("Background1.jpg", 1600 / 2, 800 / 2);
-		picture2 = EZ.addImage("Background2.jpg", (1600 / 2) + 1600, 800 / 2);
+		picture1 = EZ.addImage("Background1.jpg", x / 2, y / 2);
+		picture2 = EZ.addImage("Background2.jpg", (x / 2) + x, y / 2);
 	}
 
 	void move() {
 		picture1.moveForward(-2);
 		picture2.moveForward(-2);
-		if (picture1.getXCenter() < -800) {
-			picture1.moveForward(3200);
-		} else if (picture2.getXCenter() < -800) {
-			picture2.moveForward(3200);
+		if (picture1.getXCenter() < -y) {
+			picture1.moveForward(x*2);
+		} else if (picture2.getXCenter() < -y) {
+			picture2.moveForward(x*2);
 		}
 	}
 }
