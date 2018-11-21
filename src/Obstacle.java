0b73e5b359797bc87e04bd12ad6547eb78b031ab
Colin Jackson <cj@dhcp-168-105-242-17.wireless.manoa.hawaii.edu>
2018-11-20 20:50:49 -1000

    updated obstacle class

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -7,38 +7,38 @@ public class Obstacle {
 								   // be used to adjust the images away from eachother.
 	private static int BOTTOM = 100;// " "
 	private Random rand;
-	EZImage[] obstacle = new EZImage[O_NUMBER];
-	int posX, posY;//Both images will share the same x coordinate but there posY will vary slightly
+	EZImage top; //The obstacle is made of a top and bottom image.
+	EZImage bottom;
+	int posX, posY, botY, topY;//Both images will share the same x coordinate but there posY will vary slightly
 				   // but are based off this single y coordinate.
 	
 	Obstacle() { //default constructor spawns image off screen.
 		posY = -500;
 		posX = -500;
-		obstacle[0] = EZ.addImage("seaweed.png", posX, posY + TOP);
-		obstacle[1] = EZ.addImage("seaweed2.png", posX, posY + BOTTOM);		
+		top = EZ.addImage("TopWeed.png", posX, posY + TOP);
+		bottom = EZ.addImage("BottomWeed.png", posX, posY + BOTTOM);		
 	}
 	
 	Obstacle(int x, int y) { // when we know where we want the image to be call this contructor
 		posX = x;
 		posY = y;
-		obstacle[0] = EZ.addImage("seaweed.png", posX, posY + TOP);
-		obstacle[1] = EZ.addImage("seaweed2.png", posX, posY + BOTTOM);	
+		botY = posY + botY;
+		topY = posY + topY;
+		top = EZ.addImage("TopWeed.png", posX, posY + TOP);
+		bottom = EZ.addImage("BottomeWeed.png", posX, posY + BOTTOM);	
 	}
 	
 	
 	 public void move(int speed) {
 		posX += speed;
-		for(int i = 0; i < obstacle.length; i++) {
-			if(i % 2 == 1) {
-				obstacle[i].translateTo(posX, posY + TOP);
-			} else {
-				obstacle[i].translateTo(posX, posY + BOTTOM);
-			}
-		}
+		top.translateTo(posX, topY);
+		bottom.translateTo(posX, botY);
 	}
 	
 	 public void adjust(int X_MAX) {
 		 posY = rand.nextInt(600) + 200;
+		 botY = posY + botY;
+		 topY = posY + topY;
 		 posX = X_MAX;
 		 move(0);//calls move function to translate to new location.
 	 }
